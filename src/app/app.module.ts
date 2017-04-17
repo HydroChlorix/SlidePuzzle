5e61525db9514a91ef6006468b715f854ed74b8b
ekkapong <ekkapong@dataroot.asia>
2017-04-17 10:33:57 +0700

    add container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MyContainerComponent } from './my-container/my-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyContainerComponent
   ],
   imports: [
     BrowserModule,
